9f14a52dd87e6fd266d29a28852e91e7b503bbb4
Ahmadali <ahmadali.azizov@outlook.com>
2018-07-20 21:58:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    return "NO, NOT SINCE 1938!"
-    elsif phrase == "I LOVE YOU GRADMA!"
+  if phrase == "I LOVE YOU GRADMA!"
     return "I LOVE YOU TOO PUMPKIN"
+    elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   else 
     return "HUH?! SPEAK UP, SONNY!"
   end
